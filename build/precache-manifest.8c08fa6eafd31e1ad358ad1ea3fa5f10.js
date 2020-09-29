ec03ab271391f2c2a1f79470faeb312e439c1818
Christian Duessel <duessel@gmail.com>
2020-09-29 17:16:45 -0300

    new gh test2

---
+++ build/precache-manifest.8c08fa6eafd31e1ad358ad1ea3fa5f10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57db45f665e1123f3883e808463e43f6",
+    "url": "/trivia-challenge/index.html"
+  },
+  {
+    "revision": "241a902b87ae3aa3f776",
+    "url": "/trivia-challenge/static/css/main.57bda086.chunk.css"
+  },
+  {
+    "revision": "2057e1a66d010067a044",
+    "url": "/trivia-challenge/static/js/2.99e78154.chunk.js"
+  },
+  {
+    "revision": "b9da430482e3f0eb230634f9fc6c20c5",
+    "url": "/trivia-challenge/static/js/2.99e78154.chunk.js.LICENSE"
+  },
+  {
+    "revision": "241a902b87ae3aa3f776",
+    "url": "/trivia-challenge/static/js/main.1832d15f.chunk.js"
+  },
+  {
+    "revision": "cad14c37d06417bb81e8",
+    "url": "/trivia-challenge/static/js/runtime-main.4449d97a.js"
+  },
+  {
+    "revision": "c35c92ee5098857f71bfd40ece54933a",
+    "url": "/trivia-challenge/static/media/logo.c35c92ee.png"
+  }
+]);
